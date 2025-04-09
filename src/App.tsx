987b8f8ed36e9f1ee4318ab8d2049724f17a0170
Obradović Adna <adna.obradovic@edu.fit.ba>
2025-04-09 09:50:12 +0200

    Setting the footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./HomePage";
+import { FAQ } from "./FAQ";
+import { Contact } from "./Contact";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/faq"element={<FAQ/>}/>
+        <Route path="/contact"element={<Contact/>}/>
+
       </Routes>
     </>
   );
