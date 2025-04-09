8a0d535bacdc8235d26e33f2416a365ee66fe465
Armina Kukrica <148153866+armina01@users.noreply.github.com>
2025-04-09 22:15:02 +0200

    Revert "Setting the footer"

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./HomePage";
-import { FAQ } from "./FAQ";
-import { Contact } from "./Contact";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/faq"element={<FAQ/>}/>
-        <Route path="/contact"element={<Contact/>}/>
-
       </Routes>
     </>
   );
