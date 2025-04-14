91e0270c945745ff028e5fb8c0e4a6dd38762637
armina01 <arminakukrica11@gmail.com>
2025-04-14 16:46:38 +0200

    Improved positioning

--- src/Components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/Components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,28 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { FaMoon } from "react-icons/fa";
 import SunIcon from "../SunIcon/SunIcon";
+import "./ThemeSwitcher.css";
 
 const ThemeSwitcher = () => {
   
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    if (isDarkMode) {
-      document.body.classList.add("dark-mode");
-    } else {
-      document.body.classList.remove("dark-mode");
-    }
   }, [isDarkMode]);
 
   return (
     <div className="theme-switcher">
       <div
         className="switch"
-        onClick={() => setIsDarkMode(!isDarkMode)}
-        style={{ cursor: "pointer", display: "flex", alignItems: "center" }}
-        >
+        onClick={() => setIsDarkMode(!isDarkMode)} >
         {isDarkMode ? <FaMoon size={20} /> : <SunIcon size={20} />}
-        <p className="switch-state" style={{ marginLeft: "10px" }}>
+        <p className="switch-state">
           {isDarkMode ? "Dark mode" : "Light mode"}
         </p>
       </div>
