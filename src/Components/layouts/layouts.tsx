4b4227887d7093732953e03ea209e28037bf94b8
armina01 <arminakukrica11@gmail.com>
2025-04-15 22:18:51 +0200

    Implemented theme switcher

--- src/Components/layouts/layouts.tsx
+++ src/Components/layouts/layouts.tsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import { Footer } from "../../Footer/Footer";
 import "./layouts.css";
 import NavBar from "../TopNavBar/TopNavBar";
-const Layout = () => {
+import { ThemeContext } from "../../Contex/ThemeContex";
+
+
+const Layout: React.FC = () => {
+  const { isDarkMode } = useContext(ThemeContext);
+
   return (
-    <>
-    
-      <div className="layout-container" data-theme="light">
-      <NavBar/>
+    <div className="layout-container" data-theme={isDarkMode ? "dark" : "light"}>
+      <NavBar />
       <main className="layout-main">
         <Outlet />
       </main>
       <Footer />
     </div>
-    </>
   );
 };
+
 export default Layout;
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from './Contex/ThemeContex';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
+    <ThemeProvider>
     <App />
+    </ThemeProvider>
   </BrowserRouter>
 );
 
