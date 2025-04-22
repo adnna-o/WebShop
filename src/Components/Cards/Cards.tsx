870d06c3e0be4c3bcfe79496ba6beeaf8ee1e49e
Obradović Adna <adna.obradovic@edu.fit.ba>
2025-04-22 15:20:46 +0200

    Edited home page

--- src/Components/Cards/Cards.tsx
+++ src/Components/Cards/Cards.tsx
@@ -2,24 +2,23 @@ import { FC } from "react";
 import "./Cards.css";
 import { FaHeart } from "react-icons/fa";
 
-// Swiper core
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 import { Pagination, Navigation } from "swiper/modules";
+import { useTranslation } from "react-i18next";
 
 export const Cards: FC = () => {
+  const { t } = useTranslation();
   return (
     <div className="cards-wrapper">
       <Swiper
         slidesPerView={1}
         spaceBetween={30}
         centeredSlides={true}
-        pagination={{
-          clickable: true,
-        }}
+        pagination={{ clickable: true }}
         navigation={true}
         modules={[Pagination, Navigation]}
         className="mySwiper"
@@ -29,23 +28,23 @@ export const Cards: FC = () => {
             <div className="product-slider">
               {[1, 2, 3, 4].map((_, i) => (
                 <div className="card" key={i}>
-                  <img
-                    src="./images/women.webp"
-                    alt="product"
-                    className="product-image"
-                  />
-                  <div className="price">
+                  <div className="product-image-wrapper">
+                    <img
+                      src="./images/women.webp"
+                      alt="product"
+                      className="product-image"
+                    />
                     <button className="btn-price">$</button>
                   </div>
                   <div className="product-info">
-                    <p className="product-category">Category</p>
-                    <p className="product-brand">Brand</p>
+                    <p className="product-category">{t("product-category")}:</p>
+                    <p className="product-brand">{t("product-brand")}:</p>
                   </div>
                   <div className="card-buttons">
                     <button className="heart-btn">
-                      <FaHeart color="#5978DC" size={18} />
+                      <FaHeart color=" #6c63ff;" size={18} />
                     </button>
-                    <button className="buy-btn">BUY</button>
+                    <button className="buy-btn">{t("buy-btn")}</button>
                   </div>
                 </div>
               ))}
