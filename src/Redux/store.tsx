512d303d5331f1f2181be613acb4921b0d961d10
armina01 <arminakukrica11@gmail.com>
2025-04-24 23:09:25 +0200

    Implemented filters

--- src/Redux/store.tsx
+++ src/Redux/store.tsx
@@ -1,14 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import filtersReducer from './slices/filterSlice';
+
 
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    filters: filtersReducer,
   },
 });
 
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+
